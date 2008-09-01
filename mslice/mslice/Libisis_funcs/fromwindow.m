c878f55c7aca1dded46d11d4df724a9133136780
Dean Whittaker <Dean Whittaker@dc7c64c3-44ed-4919-9710-14e25f40e254>
2008-09-01 16:02:42 +0000

    refs #1
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@12 dc7c64c3-44ed-4919-9710-14e25f40e254

--- mslice/mslice/Libisis_funcs/fromwindow.m
+++
@@ -1,9 +0,0 @@
-function data=fromwindow
-
-% function data=fromwindow
-% extract data from mslice ControlWindow into the command line 
-
-data=get(findobj('Tag','ms_ControlWindow'),'UserData');
-if isempty(data),
-   disp('No data set to extract from the Control Window.');
-end
--- mslice/mslice/Matlab/small.m
+++
@@ -1,4 +0,0 @@
-function xx=small(x,n)
-
-xx=x-n*round(x/n);
-
