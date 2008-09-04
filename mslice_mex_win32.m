b900240b9f5732482ac086116bd53a8a9539f7d1
Dean Whittaker <Dean Whittaker@dc7c64c3-44ed-4919-9710-14e25f40e254>
2008-09-04 09:56:00 +0000

    refs #2
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@19 dc7c64c3-44ed-4919-9710-14e25f40e254

---
+++ mslice_mex_win32.m
@@ -0,0 +1,33 @@
+function mslice_mex
+% Create mex files for all the mslice fortran routines
+% Does NOT mex ffind.c So far Radu's distribution of the dll
+% this seems to work with R2007a
+
+start_dir=pwd;
+try
+    % root directory is assumed to be that in which this function resides
+    rootpath = fileparts(which('mslice_mex'));
+    cd(rootpath)
+    % Now get to the main mslice directory
+    cd mslice
+    mex avpix_df.f
+    mex cut2d_df.f
+    mex cut3d_df.f
+    mex cut3dxye_df.f
+    mex load_spe_df.f
+    %mex ms_iris.f
+    mex slice_df.f
+    mex spe2proj_df.f
+    
+
+    cd .. 
+    cd mslice_extras
+    mex slice_df_full.f
+    
+    cd(start_dir);
+    disp('Succesfully created all required mex files from fortran.')
+    
+catch
+    disp('Problems creating mex files. Please try again.')
+    cd(start_dir);
+end
