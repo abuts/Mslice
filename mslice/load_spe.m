597a405125816cdec39072fa0092ad25a4ae9ab9
Alex Buts <Alex Buts@dc7c64c3-44ed-4919-9710-14e25f40e254>
2010-08-12 18:49:13 +0000

    refs #9 minor modifications to load spe to provide the shape of energy array while loading hdf similar to the shape of the loading ASCII. It seems that opposite transformations has been performed earlier for some reasons Why?
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@133 dc7c64c3-44ed-4919-9710-14e25f40e254

--- mslice/load_spe.m
+++ mslice/load_spe.m
@@ -102,8 +102,14 @@ if strcmpi(ext,hdf_file_str.spe_hdf_file_ext)
     %     data.S  = spe_data.S;
     %     data.ERR= spe_data.ERR;    
     %     data.en = spe_data.en;    
-       [ndet,ne]=size(data.S);    
-       en=data.en';  
+       [ndet,ne]=size(data.S); 
+       % question is what shape of en is correct? it is possible that
+       % opposite to the choosen here. 
+       if size(data.en,1)==1
+            en=data.en;  
+       else
+            en=data.en';             
+       end
        data.en=(en(2:ne+1)+en(1:ne))/2; % take median values, centres of bins
        data.det_theta=ones(ndet,1);
        libisis_failed=false;  
