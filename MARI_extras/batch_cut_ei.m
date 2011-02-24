dc0c4b83f4ef8cf262fa8358f2bf3e825a782f00
Alex Buts <Alex Buts@dc7c64c3-44ed-4919-9710-14e25f40e254>
2011-02-24 17:40:38 +0000

    refs #4 recent tinning up from MARI
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/branches/STABLE_2010@152 dc7c64c3-44ed-4919-9710-14e25f40e254

--- MARI_extras/batch_cut_ei.m
+++ MARI_extras/batch_cut_ei.m
@@ -1,32 +1,37 @@
 data_path='C:\mprogs\maridata\'
-phxfile='C:\mprogs\mslice\mari\mari_resa.phx'
+phxfile='C:\mprogs\Mslice\mslice\MARI\mari_resa.phx'
 par(1)=-200;   %min energy for cut
 par(2)=200;  %max energy for cut
 par(3)=1;  % binwidth for cut
 par(4)=0;   %min Q for cut
-par(5)=11;   %max Q for cut
+par(5)=10;   %max Q for cut
 par(6)=-200        ;   %energy for integration
 par(7)=200;  %max energy for intergration
 
-figure
+figure(99);
+clf;
 title(cat(2,'Integration between ',num2str(par(6)),'mev',' and ', num2str(par(7)),'mev'))
 hold all
-EI=150
-Tcooling=[550, 560]
+EI=534;
+Tcooling=[534, 544, 553.4183, 563.85, 573.508, 582.622, 591.911, 602.100, 611.015,...
+    620.370, 629.608,639.556, 649.557,659.757,669.214,677.431,687.048, 697.746, 706.263...
+    716.551, 726.991, 735.053, 745.649, 754.909, 763.799, 775.540, 784.731, 794.276, 802.493...
+    812.455, 821.763, 831.633, 840.621, 849.947, 861.432, 869.750, 878.955 ];
 
 data1=[];
 %cooling run
 numorstart=16029;
-numorend=16030;
+numorend=16065;
 numbers=[numorstart:1:numorend]
 for i=1:length(numbers)
     runnum=num2str(numbers(i))
+    EI=Tcooling(i)
     [data,cut,int_dat]=batch_integrate(phxfile,strcat(data_path,'MAR',runnum,'.SPE'),EI,Tcooling(i),par);
     errorbar(cut.x,cut.y,cut.e,'.')
     data1=cat(1,data1,int_dat);
     
 end
-% cooling =data1;
+ cooling =data1;
 % data1=[];
 % %heating run
 % numorstart=15841;
@@ -42,7 +47,8 @@ end
 %  heating=data1;
 % 
 %plots 
-figure
+figure(100);
+clf;
 title(cat(2,'Integration between ',num2str(par(6)),'mev',' and ', num2str(par(7)),'mev'))
 hold on
 errorbar(Tcooling,(cooling(:,1)),(cooling(:,2)),'bo-')
