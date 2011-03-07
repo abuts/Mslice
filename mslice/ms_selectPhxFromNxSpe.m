427a0e2c440ae073439d5bcf4bda00a78a1a4ab4
Alex Buts <Alex Buts@dc7c64c3-44ed-4919-9710-14e25f40e254>
2011-03-07 09:04:10 +0000

    refs #10 loading phx from nxspe: user have choice what to load (old par file or par from nxspe) and the warning is issued when nxspe option is chosen (at Mantid does not work properly in all cases)
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@169 dc7c64c3-44ed-4919-9710-14e25f40e254

--- mslice/ms_selectPhxFromNxSpe.m
+++ mslice/ms_selectPhxFromNxSpe.m
@@ -1,13 +1,19 @@
 function output_args = ms_selectPhxFromNxSpe( )
 % function controls the state of the PhxFromNxSpe checkbox
-% and store its state for future usage
+% and should be storing this for future usage
+% As nxspe does not currently work properly in all situations, 
+% a warning is issued when the option is enabled
 
-warning('mslice::get_phx from nxspe: This option has not been implemented yet as nxspe phx is valid for 1:1 masks only');
 
 h_cw     =findobj('Tag','ms_ControlWindow');        
 h_checkbox=findobj(h_cw,'Tag','ms_usePhxFromNXSPE');
 Value=get(h_checkbox,'Value');
-set(h_checkbox,'Value',false);
+
+if Value
+warning('mslice:get_phx from nxspe: %s','  Getting phx from nxspe currently works correctly for 1:1 maps only');
+end
+
+% set(h_checkbox,'Value',false);
 
 end
 
