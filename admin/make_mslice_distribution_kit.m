fae8a147f66c478b31bf36a97d717a4e70dbf405
Alex Buts <Alex Buts@dc7c64c3-44ed-4919-9710-14e25f40e254>
2012-09-17 16:33:05 +0000

    refs #9 minor changes to make_mslice_distribution_kit.m
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@242 dc7c64c3-44ed-4919-9710-14e25f40e254

--- admin/make_mslice_distribution_kit.m
+++ admin/make_mslice_distribution_kit.m
@@ -29,7 +29,7 @@ rootpath = fileparts(which('mslice_init')); % MUST have rootpath so that libisis
 % end
 %
 current_dir  = pwd;
-root_dir     = current_dir;
+root_dir     = rootpath;
 % if inside herbert package dir, go avay from there:
 if strncmpi(root_dir,current_dir,numel(current_dir))
 	cd(rootpath);
