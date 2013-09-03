7072f51ad180eaa6a424a62cb530cd2a8131b6fa
Toby Perring <Toby Perring@dc7c64c3-44ed-4919-9710-14e25f40e254>
2013-09-03 16:48:03 +0000

    Correct error of a sign reversal of azimuthal angle in par files
    refs #9
    
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@252 dc7c64c3-44ed-4919-9710-14e25f40e254

--- mslice/IO/phx2par.m
+++ mslice/IO/phx2par.m
@@ -1,25 +1,42 @@
 function phx2par(phxfile,parfile,l2)
+% Convert a .phx file into a .par format 
+%
+%   >> phx2par (phxfile, parfile, l2)
+%
+% Change history:
+%   T.G.Perring   29 Aug 2013:
+%   Corrected to reverse sign in the convention for azimuthal angle
+%   Write out more significant figures
 
-% programme to convert a .phx file into a .par format 
+% phx=load_phx(phxfile);
+% if isempty(phx),
+% 	return
+% end
 
-phx=load_phx(phxfile);
-if isempty(phx),
-	return
-end
+% Read data from file
+%--------------------
+f1=fopen(phxfile,'rt');
+n=fscanf(f1,'%f \n',1);
+data=fscanf(f1,'%f');
+fclose(f1);
+data=reshape(data,7,n)';
 
-theta=phx(:,2);	% scattering angle (deg)
-psi=phx(:,3);		% azimuthal angle (deg)
-dtheta=phx(:,4)*pi/180;		% detector width (deg -> radians)
-dpsi=phx(:,5)*pi/180;		% detector height (deg -> radians)
-d=l2.*ones(size(theta));
+% Convert input
+% -------------
+group=data(:,7);            % Detector group number
+theta=data(:,3);            % scattering angle (deg)
+psi=data(:,4);              % azimuthal angle (deg)
+dtheta=data(:,5)*pi/180;    % detector width (deg -> radians)
+dpsi=data(:,6)*pi/180;		% detector height (deg -> radians)
+x2=l2.*ones(size(theta));
 
-w=2*d.*tan(dtheta/2);	% detector width (m)
-h=2*d.*tan(dpsi/2);	% detector height (m)
+w=2*x2.*tan(dtheta/2);      % detector width (m)
+h=2*x2.*tan(dpsi/2);        % detector height (m)
 
-par=[d';theta';psi';dtheta';dpsi']';
+par=[x2';theta';-psi';w';h';group'];    % *** NOTE sign reversal of psi (added TGPerring 29 Aug 2013)
 
 fid=fopen(parfile,'wt');
-fprintf(fid,'%5d \n',size(par,1));
-fprintf(fid,'%10.3f%10.3f%10.3f%10.3f%10.3f\n',par');
-disp(['Saving information for ' num2str(size(par,1)) ' detectors to .par file : ' parfile]);
+fprintf(fid,'%d \n',n);
+fprintf(fid,'%10.4f %10.4f %10.4f %10.4f %10.4f %8d \n',par);
 fclose(fid);
+disp(['Saved information for ' num2str(n) ' detectors to .par file : ' parfile]);
