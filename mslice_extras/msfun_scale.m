f514ca130c6ac44da50c2040967a4ee148bc251b
Alex Buts <Alex Buts@dc7c64c3-44ed-4919-9710-14e25f40e254>
2014-03-12 13:07:35 +0000

    refs #23 Deleted all files from old locations
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/branches/Mslice2014@266 dc7c64c3-44ed-4919-9710-14e25f40e254

--- mslice_extras/msfun_scale.m
+++
@@ -1,8 +0,0 @@
-function msfun_scale(scale)
-% Multiply dataset by scale factor:
-%
-%   >> msfun_scale(scale)
-d=fromwindow;
-d.S = d.S.*scale;
-d.ERR = d.ERR.*scale;
-towindow(d);
--- mslice_init.m
+++ mslice_init.m
@@ -17,40 +17,17 @@ function mslice_init
 rootpath = fileparts(which('mslice_init'));
 addpath(rootpath)  % MUST have rootpath so that mslice_init, mslice_off included
 addpath(fullfile(rootpath,'admin'));
-addpath(fullfile(rootpath,'ISIS_utilities'));
-% compatibility and interpackage dependencies directory
 
-% this option is disabled as we do not bind mslice and libisis any more
 
-% if exist('libisis_init.m','file')   % if libisis is on the path we populate ISIS_utilities with contents from Libisis
-%     try % this is to deal with problem of really old version of libisis initiated on the machine
-%      libisis_ver=libisis_version('number');
-%      last_copied_ver=get(mslice_config,'last_copied_libisis');
-%      if libisis_ver>last_copied_ver % libisis_ver==last_copied_ver this is the Libisis version which supports this feature
-%         path = fileparts(which('libisis_init.m'));
-%         source_path=find_path([path,filesep,'ISIS_utilities']);
-%         filelist=copy_files_list(source_path,[rootpath,'/ISIS_utilities/']);
-%         sucsess=numel(filelist);
-%         if ~sucsess
-%             warning(messageID,' Error copying libisis-dependant mslice function, Reason: %s',message);
-%         else
-%             disp(' New Libisis version has been found on the machine and Libisis-defined functions from ISIS_utilites folder have been copied to Mslice');
-%             set(mslice_config,'last_copied_libisis',libisis_ver);            
-%         end
-%      end
-%     catch
-%         warning('MSLICE:OldEnvironmnet',...
-%                  [' You have very old Libisis initiated on your computer;',...
-%                  ' It may prevent this program from working properly',...
-%                  ' You adwised to update your Libisis version to more recent version']);
-%     end
-% end
 % Other directories
-addpath_message (rootpath,'mslice');
-addpath_message (rootpath,'mslice_extras');
-addpath_message (rootpath,'mslice_more_extras');
-addpath_message (rootpath,'DLL');
+addpath_message(fullfile(rootpath,'core'));
+addpath_message (rootpath,'utilities');
+addpath_message (rootpath,'classes');
+addpath_message (rootpath,'applications');
+
+% compatibility and interpackage dependencies directory
 % temporary option used during debugging and transition to herbert io
+
 herbert_io('-off');
 % developer machine option
 if get(mslice_config,'enable_unit_tests')
@@ -63,7 +40,7 @@ if get(mslice_config,'enable_unit_tests')
             warning('MSLICE:init','can not initate unit tests framework requested by configuration');
         end
     end
-    addpath_message(rootpath,'admin','_unit_test');
+    addpath_message(rootpath,'_test');
 end
 
 disp('!==================================================================!')
@@ -71,7 +48,7 @@ disp('!                      MSLICE                                      !')
 disp('!------------------------------------------------------------------!')
 disp('!  Radu Coldea    1998-2001                                        !')
 disp('!  Various ISIS modifications and enhancements added               !')
-disp('!  Last ISIS substantial modifications date: 18/11/2013            !')
+disp('!  Last ISIS substantial modifications date: 01/05/2014            !')
 disp('!------------------------------------------------------------------!')
 
 
