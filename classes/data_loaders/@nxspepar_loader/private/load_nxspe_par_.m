e1222b3e98c348e7c24db9467e46469833c61559
wkc26243 <wkc26243@dc7c64c3-44ed-4919-9710-14e25f40e254>
2020-10-05 15:04:12 +0000

    issue with conflicting herbert and mslice load_nxspe_par_
    git-svn-id: https://svn.isis.rl.ac.uk/mslice/trunk@352 dc7c64c3-44ed-4919-9710-14e25f40e254

--- classes/data_loaders/@nxspepar_loader/private/load_nxspe_par_.m
+++ classes/data_loaders/@nxspepar_loader/private/load_nxspe_par_.m
@@ -11,7 +11,7 @@ function [par,obj] = load_nxspe_par_(obj,return_array,force_reload,getphx)
 root_folder=obj.nexus_root_dir_;
 file_name  =obj.par_file_name;
 nxspe_ver1 = false;
-if get(herbert_config,'log_level')>-1
+if get(mslice_config,'log_level')>-1
     if strncmpi(obj.nxspe_version_,'1.0',3)
         warning('LOAD_NXSPE:old_version',...
             ' you are loading detector data from partially supported nxspe data file version 1.0. For this version you should use ASCII par file instead');
@@ -46,13 +46,16 @@ else
     par(4,:) = 2*dist.*tand(0.5*d_pol); % get detector's height according to Toby's definition
     par(5,:) = 2*dist.*sind(polar).*tand(0.5*d_azim); % get detector's width according to Toby's definition
     
-    if get(herbert_config,'log_level')>1
+    if get(mslice_config,'log_level')>1
         disp(['LOADER_NXSPE:load_par::loaded ' num2str(n_det) ' detector(s)']);
     end
     obj.det_par_ = get_hor_format(par,file_name);
 end
 
 if return_array
+    if isstruct(par)
+        par = get_hor_format(par,'');    
+    end
     if getphx
         par = a_detpar_loader_interface.convert_par2phx(par);
     end
